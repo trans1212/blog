88bfd834637a8bd89b3eb4260f98475600522dda
gz-c <token@protonmail.com>
2018-01-18 18:59:31 +0800

    Deploy blog files to www.skycoin.net/blog/

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Deploy to blog.skycoin.net
+s3cmd --access_key="${AWS_ACCESS_KEY_ID}" --secret_key="${AWS_SECRET_ACCESS_KEY}" --region=ap-southeast-1 sync --delete-removed --acl-public --guess-mime-type --no-mime-magic --recursive public/ s3://blog.skycoin.net/
+
+# Deploy to www.skycoin.net/blog
+s3cmd --access_key="${AWS_ACCESS_KEY_ID}" --secret_key="${AWS_SECRET_ACCESS_KEY}" --region=ap-southeast-1 sync --delete-removed --acl-public --guess-mime-type --no-mime-magic --recursive public/ s3://www.skycoin.net/blog/
